107684774159cb5eef8eb2f8a20c59ac5be8195a
SameeraDayaratne <85645373+SameeraDayaratne@users.noreply.github.com>
2023-12-26 01:06:12 +0530

    Most popular page implemented

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -3,7 +3,7 @@
 import React from "react";
 import {useNavigate} from 'react-router-dom'
 
-function MovieCard({ imgSrc, ...props }) {
+function MovieCard({ title , posterPath }) {
   const navigate = useNavigate();
 
   function handleNavigate(){
@@ -11,28 +11,26 @@ function MovieCard({ imgSrc, ...props }) {
   }
 
   return (
-    <div>
-      <div
-        {...props}
-        className="relative sm:max-w-[180px] sm:max-h-[260px] overflow-hidden  shadow-lg group cursor-pointer"
-        onClick={handleNavigate}
-      >
-        <img
-          src={imgSrc}
-          alt=""
-          className="transition-transform group-hover:scale-110 duration-200"
-        />
-        <div className="absolute inset-0 flex items-end text-xs gap-2 bg-gradient-to-t pb-2 pl-2 from-black/60 to-transparent">
-          <div className="py-2/3 px-2 rounded-lg text-white text-[10px] bg-[#ff5100]">
-            Action
-          </div>
-          <div className="py-2/3 px-2 rounded-lg text-white text-[10px] bg-[#ff5100]">
-            Sci Fi
+    <div className="flex">
+      <div className="card sm:max-w-[180px] p-1">
+        <div className="relative sm:max-w-[180px] sm:max-h-[260px] overflow-hidden  shadow-lg group cursor-pointer" onClick={handleNavigate}>
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${posterPath}`}
+            alt=""
+            className="transition-transform group-hover:scale-110 duration-200"
+          />
+          <div className="absolute inset-0 flex items-end text-xs gap-2 bg-gradient-to-t pb-2 pl-2 from-black/60 to-transparent">
+            <div className="py-2/3 px-2 rounded-lg text-white text-[10px] bg-[#ff5100]">
+              Action
+            </div>
+            <div className="py-2/3 px-2 rounded-lg text-white text-[10px] bg-[#ff5100]">
+              Sci Fi
+            </div>
           </div>
         </div>
-      </div>
-      <div className=" mt-2">
-        <h2 className="text-white">Venom</h2>
+        <div className="mt-2">
+          <h2 className="text-white whitespace-nowrap text-ellipsis overflow-hidden">{title}</h2>
+        </div>
       </div>
     </div>
   );
