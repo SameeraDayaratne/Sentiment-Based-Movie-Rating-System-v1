b7a564038c5ebf9ca540cd7cc0ae8d24aebce405
SameeraDayaratne <85645373+SameeraDayaratne@users.noreply.github.com>
2023-12-28 23:57:42 +0530

    Started implementing review section

---
+++ client/src/components/Review.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Review(props) {
+    return (
+        <div className='w-full h-40 bg-gray-400 rounded-md'>
+            
+        </div>
+    );
+}
+
+export default Review;
