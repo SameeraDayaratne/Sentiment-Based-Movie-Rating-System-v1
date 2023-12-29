c4b7f2732a2e59474b88d8a20d29cf080c3e9446
SameeraDayaratne <85645373+SameeraDayaratne@users.noreply.github.com>
2023-12-29 12:53:01 +0530

    Implemented expand icon and comment rendering

---
+++ client/src/components/ReviewComment.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ReviewComment({children}) {
+    return (
+        <div>
+            <p>{children}</p>
+        </div>
+    );
+}
+
+export default ReviewComment;
